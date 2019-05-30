b02a1b83b96ea99b854715fecb452308e64e166e
win <win@192.168.127.52>
2019-05-30 16:02:46 +0800

    3.21

--- test3/sad/test3.java
+++ test3/sad/test3.java
@@ -2,10 +2,13 @@ package sad;
 
 public class test3 {
 	public static void main(String[] args) {
+		system.out.println("ddddddddddddddddd");
 		System.out.println("aaa");
 		System.out.println("bbb");
 		System.out.println(233355656565);
 		
 		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa");
+		system.out.println("ddddddddddddddddd");
+		
 	}
 }
